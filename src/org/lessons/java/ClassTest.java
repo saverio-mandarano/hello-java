428bcf0b6f71224eff85e2a4c92a1221c0d8f1e5
saverio mandarano <saverio.mandarano1@gmail.com>
2026-04-02 16:13:17 +0200

    add class StampaSaluto con messaggio di sluto nel metodo main

--- src/org/lessons/java/ClassTest.java
+++
@@ -1,8 +0,0 @@
-package org.lessons.java;
-
-public class ClassTest {
-    public static void main(String[] args) {
-        System.out.println("io sono la classe di prova");
-    }
-
-}
---
+++ src/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,12 @@
+package org.lessons.java;
+
+public class StampaSaluto {
+    public static void main(String[] args) {
+        System.out.println("Hello world! ");
+    }
+
+}
+
+// aggiungere la classe StampaSaluto
+// aggiungere il metodo main all’interno della classe appena creata
+// nel metodo main aggiungere l’espressione per stampare in console un messaggio di saluto
