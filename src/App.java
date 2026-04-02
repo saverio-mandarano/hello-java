d2f517d017fdb2486f3b2253adf281d9e5cf088c
saverio mandarano <saverio.mandarano1@gmail.com>
2026-04-02 16:07:11 +0200

    hello-java

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    // public static void main(String[] args) throws Exception {
+    //     System.out.println("Hello, Saverio!");
+    // }
+    public static void main(String[] args) {
+        System.out.println("stringa appena scritta2");
+        System.out.println("stringa appena scritta3");
+    }
+}
---
+++ src/org/lessons/java/ClassTest.java
@@ -0,0 +1,8 @@
+package org.lessons.java;
+
+public class ClassTest {
+    public static void main(String[] args) {
+        System.out.println("io sono la classe di prova");
+    }
+
+}
